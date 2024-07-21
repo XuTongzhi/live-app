d3ff1f6f0cb438111d1899301de0eac4292580e7
xtz <884136445@qq.com>
2024-07-21 19:44:50 +0800

    用户中台开发

---
+++ qiyu-live-common-interface/src/main/java/org/qiyu/live/common/interfaces/topic/UserProviderTopicNames.java
@@ -0,0 +1,14 @@
+package org.qiyu.live.common.interfaces.topic;
+
+/**
+ * @Author idea
+ * @Date: Created in 16:14 2024/7/12
+ * @Description
+ */
+public class UserProviderTopicNames {
+
+    /**
+     * 专门处理和用户信息相关的缓存延迟删除操作
+     */
+    public static final String CACHE_ASYNC_DELETE_TOPIC = "UserCacheAsyncDelete";
+}
